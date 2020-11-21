7d7c91c0ac82f7481f0720c311cb00b4b39e8f6e
erinszabo <55733370+erinszabo@users.noreply.github.com>
2020-11-20 18:38:43 -0800

    added stuff

--- sorts.py
+++ sorts.py
@@ -1,11 +1,36 @@
 
 
 def bubble_sort(alist):
-    for passnum in range(len(alist)-1, 0, -1):
+    for passnum in range(len(alist) - 1, 0, -1):
         for i in range(passnum):
-            if alist[i]>alist[i+1]:
+            if alist[i] > alist[i + 1]:
                 temp = alist[i]
-                alist[i] = alist[i+1]
-                alist[i+1] = temp
+                alist[i] = alist[i + 1]
+                alist[i + 1] = temp
     return alist
 
+
+def shortBubbleSort(alist):
+    exchanges = True
+    passnum = len(alist) - 1
+    while passnum > 0 and exchanges:
+        exchanges = False
+        for i in range(passnum):
+            if alist[i] > alist[i + 1]:
+                exchanges = True
+                temp = alist[i]
+                alist[i] = alist[i + 1]
+                alist[i + 1] = temp
+        passnum = passnum - 1
+
+
+def selectionSort(alist):
+    for fillslot in range(len(alist)-1, 0, -1):
+        positionOfMax = 0
+        for location in range(1, fillslot + 1):
+            if alist[location] > alist[positionOfMax]:
+                positionOfMax = location
+
+        temp = alist[fillslot]
+        alist[fillslot] = alist[positionOfMax]
+        alist[positionOfMax] = temp
